e9461d346b2c472eb6bb6c91ce36081f650662a1
ValeriaStav <100225737+ValeriaStav@users.noreply.github.com>
2022-08-22 21:43:55 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -3,42 +3,42 @@ GO
 
 --valid cases
 
-INSERT INTO Customers (FirstName, LastName, CustomerPhoneNumber, CustomerEmail, TotalPurchaseAmount)
+INSERT INTO Customer (FirstName, LastName, CustomerPhoneNumber, CustomerEmail, TotalPurchaseAmount)
 VALUES ('John', 'Doe', '1234567890', 'JohnD555@gmail.com', 10000);
 
-INSERT INTO Customers (FirstName, LastName, CustomerPhoneNumber, CustomerEmail, TotalPurchaseAmount)
+INSERT INTO Customer (FirstName, LastName, CustomerPhoneNumber, CustomerEmail, TotalPurchaseAmount)
 VALUES (NULL,'Black', NULL, NULL, NULL);
 
-INSERT INTO Customers (LastName)
+INSERT INTO Customer (LastName)
 VALUES ('White');
 
-INSERT INTO Addresses (CustomerId, AddressLine, AddressLine2, AddressType, City, PostalCode, [State], Country)
+INSERT INTO Address (CustomerId, AddressLine, AddressLine2, AddressType, City, PostalCode, [State], Country)
 VALUES (1, 'First line', 'Second line', 'Shipping', 'Ottawa', '123456', 'Ontario', 'Canada')
 
-INSERT INTO Notes(Note, CustomerId)
+INSERT INTO Note (Note, CustomerId)
 VALUES ('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.', 1);
 
 
 
 --invalid cases
 
-iNSERT INTO Customers (FirstName, LastName, CustomerPhoneNumber, CustomerEmail, TotalPurchaseAmount)
+iNSERT INTO Customer (FirstName, LastName, CustomerPhoneNumber, CustomerEmail, TotalPurchaseAmount)
 VALUES ('John', NULL, '1234567890', 'JohnD555@gmail.com', 10000);
 
-INSERT INTO Customers (FirstName)
+INSERT INTO Customer (FirstName)
 VALUES ('Joeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
 
-INSERT INTO Customers (LastName)
+INSERT INTO Customer (LastName)
 VALUES ('Blackkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
 
-INSERT INTO Customers (LastName)
+INSERT INTO Customer (LastName)
 VALUES (NULL);
 
-iNSERT INTO Customers (CustomerPhoneNumber)
+iNSERT INTO Customer (CustomerPhoneNumber)
 VALUES ('123456789#0987654321');
 
-iNSERT INTO Customers (CustomerEmail)
+iNSERT INTO Customer (CustomerEmail)
 VALUES ('JohnD555#gmail,c');
 
-INSERT INTO Addresses (CustomerId, AddressLine, AddressLine2, AddressType, City, PostalCode, [State], Country)
+INSERT INTO Address (CustomerId, AddressLine, AddressLine2, AddressType, City, PostalCode, [State], Country)
 VALUES (3, 'First line', 'Second line', 'Chipping', 'Ottawa', '1234567', 'Ontarioooooooooooooooooooooooooo', 'Africa')
